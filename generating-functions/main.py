c91a6e980c4aa166dd1634ea116317b132415efc
r <rharv99@gmail.com>
2024-06-10 11:10:21 -0400

    sync

--- generating-functions/main.py
+++ generating-functions/main.py
@@ -1,7 +1,26 @@
 from latextool_basic import *
+from latexcircuit import *
+from labeledpoint import *
+L = POLY_LINEWIDTH
 p = Plot()
-p += Rect(0, 0, 12, 2)
-p += Rect(0, 1, 2, 2, background='blue!20!white')
-p += Rect(0, 0, 2, 1, background='blue!20!white')
+V = regularpoly(6)
+for i in [0]:
+    V1 = V[:]
+    V1 = [(x+i*3.25, y) for (x,y) in V1]
+    p += polygon(points=V1,
+                fill=False,
+                drawboundary=True,
+                closed=True,
+                )
+    p += polygon(points=[V1[4],V1[5],V1[i]],
+                fill=False, linewidth=0.01,
+                drawboundary=True,
+                closed=True,
+                )
+    p += polygon(points=[V1[0],V1[1],V1[3]],
+                fill=False, linewidth=0.01,
+                drawboundary=True,
+                closed=True,
+                )
 print(p)
 
