4ede3743f04d89c9c208794a6a81fc204174d43c
Ryan Harvey <rharv99@gmail.com>
2024-10-26 21:01:00 -0400

    sync

--- python/assignment_tool/ayo.py
+++ python/assignment_tool/ayo.py
@@ -2,7 +2,9 @@ import config
 import os
 
 def main():
-    s = "\\input{thispreamble.tex}\n\n\\newcommand\\myincludetex[1]{\\textbox{{\\scriptsize \\texttt{#1}}}\n\n  \\input{#1}\n\n}\n\n\\newcommand\\myincludesrc[1]{\\textbox{{\\scriptsize \\texttt{#1}}}\n\n  \\VerbatimInput[fontsize=\\footnotesize,frame=single]{#1}\n\n}\n"
+    s = '''\\input{thispreamble.tex}\n\n\\newcommand\\myincludetex[1]{\\textbox{{\\scriptsize \\texttt{#1}}}\n\n
+    \\input{#1}\n\n}\n\n\\newcommand\\myincludesrc[1]{\\textbox{{\\scriptsize \\texttt{#1}}}\n\n
+    \\VerbatimInput[fontsize=\\footnotesize,frame=single]{#1}\n\n}\n'''
 
     for i in config.dir:
         s = s + "\\newpage\n\\input{" + i + '}\n\n'
