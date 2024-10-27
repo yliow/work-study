67439e9e8765b6a133241a02bf26342ea1e5addf
Oyedeji <robertoyedeji7@gmail.com>
2024-10-27 15:11:37 -0700

    10/27/2024

---
+++ assignment_tool/ciss358/a02-test/config.py
@@ -0,0 +1,16 @@
+# depends on file structure
+name = "yliow"
+courses = "ciss358"
+assignment = "2"
+
+dir = [("other/objective.tex", ''),
+       ("other/induction.tex", 'nln'),
+       ("a02q01/q01.tex", 'nln'),
+       ("a02q01/q01s.tex", 'sln'),
+       ("other/primes.tex", 'nln'),
+       ("a02q02/q02.tex", 'nln'),
+       ("a02q02/q02s.tex", 'sln'),
+       ("a02q03/q03.tex", 'nln'),
+       ("a02q03/q03s.tex", 'sln')
+       ]
+
