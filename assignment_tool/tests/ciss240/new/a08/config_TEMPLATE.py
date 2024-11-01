49c7965363e64f7276d764f657b9340fe654c29b
nalahaideb <nalahaideb1@cougars.ccis.edu>
2024-11-01 15:52:07 -0700

    xd

---
+++ assignment_tool/tests/ciss240/new/a08/config_TEMPLATE.py
@@ -0,0 +1,16 @@
+#              THIS IS A TEMPLATE FOR THE "config.py" file!!
+#              LOOK AT PDF TO DETERMINE THE STRUCTURE OF "config.py"
+###########################################################################
+
+# depends on file structure
+name = "yliow"
+courses = "ciss350"
+assignment = "a01"
+
+# nln or sln is to determine whether to put a new page at the begining or
+# or a solution
+
+dir = [("a01q01/question/main.tex", 'nln'),
+       ("a01q02/question/main.tex", 'nln'),
+       ("a01q03/question/main.tex", 'nln')]
+###########################################################################
