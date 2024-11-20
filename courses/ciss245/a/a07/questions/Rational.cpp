44b026537854dc19ee1494ee32d5df39b22a980b
Yihsiang Liow <yliow@ccis.edu>
2024-11-20 01:18:24 -0500

    no msg

---
+++ courses/ciss245/a/a07/questions/Rational.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Rational.h"
+
+std::ostream & operator<<(std::ostream & cout, const Rational & r)
+{
+    // TODO
+    return cout;
+}
+
+
+std::istream & operator>>(std::istream & cin, Rational & r)
+{
+    // TODO
+    return cin;
+}
