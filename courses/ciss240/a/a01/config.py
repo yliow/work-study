27bf11440adfea9982345d7693d45befe0250b11
Oyedeji <robertoyedeji7@gmail.com>
2024-11-27 18:26:31 -0800

    11/27/2024

--- courses/ciss240/a/a01/config.py
+++ courses/ciss240/a/a01/config.py
@@ -12,7 +12,7 @@ courses = "ciss240"
 name = "yliow"
 assignment = "a01"
 assignment_it = "1";
-basepath = "test/ciss240/" # insert path for new directory (you are able to go back)
+basepath = "New/" # insert path for new directory (you are able to go back)
 destination = basepath + assignment
 # basepath = 
 # go to basepath, write, then go back to cwd
@@ -27,19 +27,19 @@ destination = basepath + assignment
 # - skel is the skelenton code that needs to be in the 
 # - other is any other latex files that needs to be included
 # - name of file
-contents = [(at.OTHER, "../../courses/ciss240/a/a01/objectives.tex"),
+contents = [(at.OTHER, "old/objectives.tex"),
             (at.LATEXSTR, r"\newpage"),
-            (at.OTHER, "../../courses/ciss240/a/a01/instructions.tex"),
+            (at.OTHER, "old/instructions.tex"),
             (at.LATEXSTR, r"\newpage"),
-            (at.QUEST_CODE, "../../courses/ciss240/a/a01/a01q01/question/main.tex"),
+            (at.QUEST_CODE, "old/q01.tex"),
             (at.LATEXSTR, r"\newpage"),
-            (at.QUEST_CODE, "../../courses/ciss240/a/a01/a01q02/question/main.tex"),
+            (at.QUEST_CODE, "old/q02.tex"),
             (at.LATEXSTR, r"\newpage"),
-            (at.QUEST_CODE, "../../courses/ciss240/a/a01/a01q03/question/main.tex"),
+            (at.QUEST_CODE, "old/q03.tex"),
             (at.LATEXSTR, r"\newpage"),
-            (at.QUEST_CODE, "../../courses/ciss240/a/a01/a01q04/question/main.tex"),
+            (at.QUEST_CODE, "old/q04.tex"),
             (at.LATEXSTR, r"\newpage"),
-            (at.QUEST_CODE, "../../courses/ciss240/a/a01/a01q05/question/main.tex"),
+            (at.QUEST_CODE, "old/q05.tex"),
             (at.LATEXSTR, r"\newpage")
             ]
 ###########################################################################
