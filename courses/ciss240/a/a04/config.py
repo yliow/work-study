3203b0a816170c5545e3c7bd26995331e54bb881
Oyedeji <robertoyedeji7@gmail.com>
2024-12-21 12:27:36 -0800

    config

--- courses/ciss240/a/a04/config.py
+++ courses/ciss240/a/a04/config.py
@@ -22,11 +22,17 @@ destination = basepath + assignment
 
 #latex
 # contents[i][0] is the type of documents we are copying
-# - QUESTION is a coding question
-# - question is a math question that needs a lates answer file
-# - skel is the skelenton code that needs to be in the 
-# - other is any other latex files that needs to be included
-# - name of file
+# - QUEST_CODE is a coding question
+# - QUEST_MATH is a math question that needs a lates answer file
+# - SKELETON is the skelenton code that will be called by one of the files
+# - SKEL_PDF is the skelenton code that will not be called by one of the
+#   files but included on the pdf
+# - OTHER is any other latex files that needs to be included
+# - DOCUMENTS is any file that need to be in the pdf and is particular
+#   to an assignment question and is included in the pdf
+# - EXTRA_DOCUMENTS is same as DOCUMENTS but it appearance in the pdf
+#   depends on assignment question
+
 contents = [(at.OTHER, "Old/objective.tex"),
             (at.LATEXSTR, r"\newpage"),
             (at.OTHER, "Old/instruction.tex"),
