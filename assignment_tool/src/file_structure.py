6b0cdae1a6d0cc6bf60d01e3c27fb4ab1b8e7bf7
OladayoOyedeji <robertoyedeji7@gmail.com>
2025-01-14 16:36:11 -0600

    Update file_structure.py

--- assignment_tool/src/file_structure.py
+++ assignment_tool/src/file_structure.py
@@ -8,20 +8,17 @@ def get_num():
     for i in con.contents:
         if i[0] == 'question' or i[0] == 'QUESTION':
             num += 1
-
     return num
 
 def file_struct():
     if os.path.exists(con.destination):
         print(con.destination)
         answer = input("^ this path already exist are you sure you want to overwrite it? enter [y/n]: ")
-        if answer == "y" or answer == "yes" or answer == "yeah":
-            shutil.rmtree(con.destination)
-        else:
+        if answer not in ["y", "yes", "yeah"]:
             return False
-    
+        else:
+            shutil.rmtree(con.destination)
     num = get_num()
     path = rf"{con.destination}/{con.assignment}"
-    
     at.FILE_VERSION(path, num)
     return True
