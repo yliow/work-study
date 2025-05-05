bd7eadb950bf1c78acacf9259683b6c298218bd4
rharv99@gmail.com <rharv99@gmail.com>
2025-05-05 10:36:29 -0700

    sync

--- test-discrete-probability/main.py
+++ test-discrete-probability/main.py
@@ -1,23 +1,19 @@
+s = r'''
+n = 2
+while 1:
+    p = 1
+    for i in range(1, n):
+        p *= (1 - i / 366.0)
+    print(n, 1 - p)
+    if 1 - p > 0.5:
+         break
+    n += 1
+'''.strip()
+f = open('a15245236.py', 'w')
+f.write(s)
+f.close()
 from latextool_basic import *
-p = Plot()
-edges={'7':['3','9'],
- '3':['1','4'],
- '1':['0','2'],
- '4':['5'],
- '5':['','6'],
- '9':['8']}
-pos = bintreepositions(edges=edges)
-
-Graph.r = 0.3
-for k,(x,y) in pos.items():
-    if k in edges.keys():
-        p += Graph.node(x=x, y=y, name=k, label=k)
-    else:
-        p += Graph.node(x=x, y=y, name=k, label=k, background='white')
-
-for k,v in edges.items():
-    for x in v:
-        if x in [None,'']: continue
-        p += Graph.arc(names=[k,x])
-print(p)
+print(r'{\small %s}' % console(s))
+print("and the output")
+print(r'{\small %s}' % shell('python a15245236.py'))
 
