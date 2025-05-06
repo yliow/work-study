b357d5f7f94af006873bfe5948d45d4fa1debb0e
rharv99@gmail.com <rharv99@gmail.com>
2025-05-06 04:39:50 -0700

    sync

--- chkbook.py
+++ chkbook.py
@@ -16,16 +16,16 @@ def check():
     if exercises_dir_present and stdout_dir_present and chapters_dir_present:
         return 'test passed (all directories are present) ...'
 
-    s = ''
+    s = 'You need '
 
     if not exercises_dir_present:
-        s += 'You need an exercises directory'
+        s += 'exercises '
     if not stdout_dir_present:
-        s += 'You need an stdout directory'
+        s += 'stdout '
     if not chapters_dir_present:
-        s += 'You need a chapters directory'
+        s += 'chapters '
 
-    return s
+    return s + 'directories'
 
 if __name__ == '__main__':
     checking_directories = check()
@@ -34,6 +34,7 @@ if __name__ == '__main__':
     if 'test passed' in checking_directories:
         print('continue doing checks')
     else:
+        print()
         print('would you like me to create ')
         if 'exercises' in checking_directories:
             print('an exercises directory? ')
@@ -41,5 +42,23 @@ if __name__ == '__main__':
             if choice != 'n':
                 print('creating exercises directory for you ...')
                 os.mkdir('exercises')
+            else:
+                print('leaving ...')
+        print()
+        if 'stdout' in checking_directories:
+            print('an stdout directory? ')
+            choice = input()
+            if choice != 'n':
+                print('creating stdout directory for you ...')
+                os.mkdir('stdout')
+            else:
+                print('leaving ...')
+        print()
+        if 'chapters' in checking_directories:
+            print('an chapters directory? ')
+            choice = input()
+            if choice != 'n':
+                print('creating chapters directory for you ...')
+                os.mkdir('chapters')
             else:
                 print('leaving ...')
