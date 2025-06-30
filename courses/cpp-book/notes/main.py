4a6cfde9bd72fbb1e219ddcb4b5b562936f59c7a
nalahaideb <nalahaideb1@cougars.ccis.edu>
2025-06-30 11:51:18 -0700

    finished tikz drawings in ch10

--- courses/cpp-book/notes/main.py
+++ courses/cpp-book/notes/main.py
@@ -1,62 +1,40 @@
 from latextool_basic import *
-p = Plot()
-p += Grid(x0=-5, y0=-7, x1=12, y1=5)
-D = '''
+from latexcircuit import *
+D = r'''
 
 
 
+int sum = 0;
+
+    for ( int i = 3;     i < 5;     i = i + 1 )
 
 
-for (         ;                ;                )
-
-
-
-
-
-
-
-
-true
-
-
-
-false
-
-
+        sum = sum + i;
 
 
 
 '''.strip()
 
-p += Line(points=[(2.1, -0.6),(4.2, -0.6)] , linewidth=0.1, linecolor='red', endstyle='>', bend_right=60)
-p += Line(points=[(-2.3, -7.6),(-0.2, -7.6)] , linewidth=0.1, linecolor='red', endstyle='>')
-p += Line(points=[(-2.3, -6.7),(-2.3, -7.6)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(10.7, -6.7),(-2.3, -6.7)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(10.7, -5.65), (10.7, -6.7)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(10, -5.65),(10.7, -5.65)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(8, -0.6),(5.6, -0.6)] , linewidth=0.1, linecolor='red', endstyle='>', bend_left=40)
-p += Line(points=[(-0.7, -4.5),(1.6, -4.5)] , linewidth=0.1, linecolor='red', endstyle='>')
-p += Line(points=[(7.5, -4.6),(8.6, -0.6)] , linewidth=0.1, linecolor='red', endstyle='>', bend_right=30)
-p += Line(points=[(-0.7, -5.65),(10, -5.65)] , linewidth=0.1, linecolor='red', endstyle='>')
-p += Line(points=[(-0.7, -3.4),(-0.7, -5.65)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(5, -3.4),(-0.7, -3.4)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(5, -0.6),(5, -3.4)], linewidth=0.1, linecolor='red')
-p += Line(points=[(-1.2, -1.5),(1.9, -0.6)] , linewidth=0.1, linecolor='red', endstyle='>', bend_right=60)
-p += Line(points=[(-2.3, -1.5),(-1.2, -1.5)] , linewidth=0.1, linecolor='red', endstyle='>')
-p += Line(points=[(-2.3, 1.1),(-2.3, -1.5)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(11, 1.1),(-2.3, 1.1)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(11, 2.1),(11, 1.1)] , linewidth=0.1, linecolor='red')
-p += Line(points=[(10, 2.1),(11, 2.1)] , linewidth=0.1, linecolor='red')
+p = Plot()
+code(p, D, x=-4.5, y=0, border_linewidth=0.05, innersep=0.6)
 
-p += Rect(-1.2, 1.6, 10, 3, linewidth=0.1)
-p += Rect(1.1, -0.6, 2.7, 0.2, linewidth=0.1)
-p += Rect(3.5, -0.6, 5.8, 0.2, linewidth=0.1)
-p += Rect(7.5, -0.6, 9.5, 0.2, linewidth=0.1)
-p += Rect(-1.2, -6.3, 10, 0.6, linewidth=0.1)
-p += Rect(1.6, -4.9, 7.5, -4.3, linewidth=0.1)
-p += Rect(-0.2, -8.5, 10.4, -7.1, linewidth=0.1)
+#p += Grid(x0=-5,y0=-10,x1=10,y1=2)
 
-code(p, D)
+p += ellipse(x0=-5,y0=-8,x1=1,y1=-4, linewidth=0.1)
+
+p += Rect(x0=3,y0=-8,x1=10,y1=-4, linewidth=0.1)
+p += Rect(x0=-4,y0=-2.9,x1=2,y1=-2.71, linewidth=0.12, linecolor='red')
+#p += Rect(x0=7.5,y0=-2,x1=11,y1=1, linewidth=0.2, linecolor='red', align='l', font='\normalsize\bfseries', s = 'When you leave the for-loop, \\ variable i is destroyed')
+
+p += Rect(x0=5,y0=-5.5,x1=8,y1=-4.5, linewidth=0.1, label='7')
+
+X = POINT(x=-2.3, y=-3.9, r=0, label='CPU', anchor='flushtopleft')
+Y = POINT(x=6.1, y=-3.9, r=0, label='main', anchor='flushtopleft')
+Z = POINT(x=5, y=-5, r=0, label='sum', anchor='east')
+
+p += str(X)
+p += str(Y)
+p += str(Z)
 
 print(p)
 
