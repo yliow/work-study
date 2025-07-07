6c4f4d6efaf761c5d709e2dda47c87cb6688c034
nalahaideb <nalahaideb1@cougars.ccis.edu>
2025-07-07 06:13:13 -0700

    more work on ch68

--- courses/cpp-book/notes/main.py
+++ courses/cpp-book/notes/main.py
@@ -1,43 +1,21 @@
 from latextool_basic import *
 p = Plot()
-#p += Grid(x0=-3,y0=-10,x1=15,y1=5)
+#p += Grid(x0=-2,y0=-5, x1=10, y1=3)
+R0 = Rect(6, -2, 15, 0.1, label =r'To make the first dimension dynamic.', linewidth=0.1, linecolor='red')
+R1 = Rect(6, -4, 15, -2, label =r'To make the second dimension dynamic', linewidth=0.1, linecolor='red')
+R2 = Rect(6, -6.1, 15, -4.1, label =r'To make the value in the 2-d array dynamic', linewidth=0.1, linecolor='red')
+L0 = Line(points=[R0.left(),(3.05,-1),(3.05,0.8)], linewidth=0.1, linecolor='red', endstyle='>')
+L1 = Line(points=[R1.left(),(2.5, R1.centery()),(2.5, float(R1.centery()) + 3.8)], linewidth=0.1, linecolor='red', endstyle='>')
+L2 = Line(points=[R2.left(),(2, R2.centery()),(2,float(R2.centery()) + 5.9)], linewidth=0.1, linecolor='red', endstyle='>')
+textbox = Rect(0, 0, 4, 2, label = r'\huge\texttt{Int *** a}', linewidth=0)
 
-G = Rect(-1, 3, 1, 5, linewidth=0.05, font=r'\huge', label=r'\texttt{G}')
-P0 = Rect(-3, -1, -0.5, 1, linewidth=0.05, font=r'\huge', label=r'\texttt{P0}')
-P1 = Rect(0.5, -1, 3, 1, linewidth=0.05, font=r'\huge', label=r'\texttt{P1}')
-C = Rect(-1, -5, 1, -3, linewidth=0.05, font=r'\huge', label=r'\texttt{C}')
-
-p += Line(points=[(-2,-1),(-2,-2),(2,-2),(2,-1),(2,-2),(0,-2),(C.top())], linewidth=0.05)
-p += Line(points=[(-2,-2),(-2,-1)], linewidth=0.05, endstyle='>')
-p += Line(points=[(2,-2),(2,-1)], linewidth=0.05, endstyle='>')
-p += Line(points=[(-2,1),(-2,2),(2,2),(2,1),(2,1),(2,2),(0,2),(G.bottom())], linewidth=0.05, endstyle='>')
-
-E0 = ellipse(5,0,8,2, linewidth=0.1, label=r'x\_')
-E1 = ellipse(9,0,12,2, linewidth=0.1, label=r'x\_')
-E2 = ellipse(5,-3,8,-1, linewidth=0.1)
-E3 = ellipse(9,-3,12,-1, linewidth=0.1)
-E4 = ellipse(7,-6,10,-4, linewidth=0.1)
-p += Line(points=[(6.5,1), (6.5,-2), (8.5,-5), (10.5,-2), (10.5,1)], linewidth=0.1)
-
-p += G
-p += Line(points=[G.right(),(6.5,1)], linewidth=0.05)
-p += Line(points=[G.right(),(10.5,1)], linewidth=0.05)
-
-p += P0
-p += Line(points=[P0.bottomright(),(6.5,-2)], linewidth=0.05)
-
-p += P1
-p += Line(points=[P1.right(),(10.5,-2)], linewidth=0.05)
-
-p += C
-p += Line(points=[C.right(),(8.5,-5)], linewidth=0.05)
-
-
-p += E0
-p += E1
-p += E2
-p += E3
-p += E4
+p += R0
+p += R1
+p += R2
+p += L0
+p += L1
+p += L2
+p += textbox
 
 print(p)
 
