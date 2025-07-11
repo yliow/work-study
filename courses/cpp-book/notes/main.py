689f2318f612b2051fb39bfaac846606e18ee42c
nalahaideb <nalahaideb1@cougars.ccis.edu>
2025-07-11 03:27:01 -0700

    updated ch80 to use consolethree

--- courses/cpp-book/notes/main.py
+++ courses/cpp-book/notes/main.py
@@ -1,40 +1,43 @@
 from latextool_basic import *
-from latexcircuit import *
-D = r'''
-
-
-
-int sum = 0;
-
-    for ( int i = 3;     i < 5;     i = i + 1 )
-
-
-        sum = sum + i;
-
-
-
-'''.strip()
-
 p = Plot()
-code(p, D, x=-4.5, y=0, border_linewidth=0.05, innersep=0.6)
+#p += Grid(x0=-3,y0=-10,x1=15,y1=5)
 
-#p += Grid(x0=-5,y0=-10,x1=10,y1=2)
+G = Rect(-1, 3, 1, 5, linewidth=0.05, font=r'\huge', label=r'\texttt{G}')
+P0 = Rect(-3, -1, -0.5, 1, linewidth=0.05, font=r'\huge', label=r'\texttt{P0}')
+P1 = Rect(0.5, -1, 3, 1, linewidth=0.05, font=r'\huge', label=r'\texttt{P1}')
+C = Rect(-1, -5, 1, -3, linewidth=0.05, font=r'\huge', label=r'\texttt{C}')
 
-p += ellipse(x0=-5,y0=-8,x1=1,y1=-4, linewidth=0.1)
+p += Line(points=[(-2,-1),(-2,-2),(2,-2),(2,-1),(2,-2),(0,-2),(C.top())], linewidth=0.05)
+p += Line(points=[(-2,-2),(-2,-1)], linewidth=0.05, endstyle='>')
+p += Line(points=[(2,-2),(2,-1)], linewidth=0.05, endstyle='>')
+p += Line(points=[(-2,1),(-2,2),(2,2),(2,1),(2,1),(2,2),(0,2),(G.bottom())], linewidth=0.05, endstyle='>')
 
-p += Rect(x0=3,y0=-8,x1=10,y1=-4, linewidth=0.1)
-p += Rect(x0=-4,y0=-2.9,x1=2,y1=-2.71, linewidth=0.12, linecolor='red')
-#p += Rect(x0=7.5,y0=-2,x1=11,y1=1, linewidth=0.2, linecolor='red', align='l', font='\normalsize\bfseries', s = 'When you leave the for-loop, \\ variable i is destroyed')
+E0 = ellipse(5,0,8,2, linewidth=0.1, label=r'x\_')
+E1 = ellipse(9,0,12,2, linewidth=0.1, label=r'x\_')
+E2 = ellipse(5,-3,8,-1, linewidth=0.1)
+E3 = ellipse(9,-3,12,-1, linewidth=0.1)
+E4 = ellipse(7,-6,10,-4, linewidth=0.1)
+p += Line(points=[(6.5,1), (6.5,-2), (8.5,-5), (10.5,-2), (10.5,1)], linewidth=0.1)
 
-p += Rect(x0=5,y0=-5.5,x1=8,y1=-4.5, linewidth=0.1, label='7')
+p += G
+p += Line(points=[G.right(),(6.5,1)], linewidth=0.05)
+p += Line(points=[G.right(),(10.5,1)], linewidth=0.05)
 
-X = POINT(x=-2.3, y=-3.9, r=0, label='CPU', anchor='flushtopleft')
-Y = POINT(x=6.1, y=-3.9, r=0, label='main', anchor='flushtopleft')
-Z = POINT(x=5, y=-5, r=0, label='sum', anchor='east')
+p += P0
+p += Line(points=[P0.bottomright(),(6.5,-2)], linewidth=0.05)
 
-p += str(X)
-p += str(Y)
-p += str(Z)
+p += P1
+p += Line(points=[P1.right(),(10.5,-2)], linewidth=0.05)
+
+p += C
+p += Line(points=[C.right(),(8.5,-5)], linewidth=0.05)
+
+
+p += E0
+p += E1
+p += E2
+p += E3
+p += E4
 
 print(p)
 
