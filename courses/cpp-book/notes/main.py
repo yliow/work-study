77d86cc1bbe49e6b577c78d8009870cbc751a482
nalahaideb <nasseralahaideb@hotmail.com>
2025-12-21 19:09:44 -0600

    work on ch26

--- courses/cpp-book/notes/main.py
+++ courses/cpp-book/notes/main.py
@@ -17,32 +17,37 @@ textbox9 = Rect(-9.9, 0.5, -9.95, 0.5, linewidth=0.0, label='\\texttt{\}}', alig
 execbox = Rect(-10.0, -3, -4.0, -2, linewidth=0.0,
                label='\\EMPHASIZE{Program execution at this point}')
 execbox0 = Rect(-10.0, -4, -4.0, -3, linewidth=0.0,
-               label='\\EMPHASIZE{(about to call f() a second time)}')
+               label='\\EMPHASIZE{(after calling f() five times)}')
 
 p += execbox
 p += execbox0
 
 membox = Rect(0.0, -1.5, 4.0, 5.5, linewidth=0.1)
 
-#markbox = Rect(5.5, -1, 8.25, 2, innersep=0.1, linewidth=0.0, s='this area is still marked as used', align='c')
-#p += markbox
+markbox = Rect(0.5, 6.5, 3.5, 7.5, innersep=0.05, linewidth=0.0, s='{\\small Five areas are now marked as used}', align='c')
+p += markbox
+
 fstore = Rect(4.5, -1.5, 8.5, 5.5, linewidth=0.1)
 fstorelabel = Rect(6.5, 7, 6.5, 7, linewidth=0.0, label='\\textbf{Free Store}')
 
 fbluebox = Rect(6, 4.5, 7.5, 5, linewidth=0.0, background='blue')
-fbox = Rect(0.5, 2.5, 3.5, 4.75, linewidth=0.1)
-flabel = Rect(0.5, 5.5, 0.5, 5.5, linewidth=0.0, label='\\textbf{f}')
+fbluebox0 = Rect(4.5, 3.5, 6, 4, linewidth=0.0, background='blue')
+fbluebox1 = Rect(4.75, 1.5, 6.25, 2, linewidth=0.0, background='blue')
+fbluebox2 = Rect(5.75, 2.5, 7.25, 3, linewidth=0.0, background='blue')
+fbluebox3 = Rect(6.5, 5.5, 8, 6, linewidth=0.0, background='blue')
 
 mbox = Rect(0.5, -1.5, 3.5, 1.25, linewidth=0.1)
 mboxlabel = Rect(1.5, 2, 1.5, 2, linewidth=0.0, label='\\textbf{main}')
 
 p += membox
-
 p += fstore
 p += fstorelabel
 p += fbluebox
-p += fbox
-p += flabel
+p += fbluebox0
+p += fbluebox1
+p += fbluebox2
+p += fbluebox3
+#p += fbluebox4
 
 p += mbox
 p += mboxlabel
@@ -59,9 +64,15 @@ p += textbox7
 p += textbox8
 p += textbox9
 
-reddot = Circle(x=-9, y=4.5, r=0.2, linecolor='red', background='red')
-p += Line(points=[(-9.25, -1.5), (-9, 4.15)], linewidth=0.15, linecolor='red', endstyle='>')
+reddot = Circle(x=-1.75, y=1.5, r=0.2, linecolor='red', background='red')
+p += Line(points=[(-9.25, -1.5), (-1.75, 1.25)], linewidth=0.1, linecolor='red', endstyle='>')
 p += reddot
+
+p += Line(points=[(2, 6.25), (5.75, 5)], linewidth=0.1, linecolor='red', endstyle='>')
+p += Line(points=[(2, 6.25), (5.75, 3.25)], linewidth=0.1, linecolor='red', endstyle='>')
+p += Line(points=[(2, 6.25), (4.75, 1.75)], linewidth=0.1, linecolor='red', endstyle='>')
+p += Line(points=[(2, 6.25), (5.25, 4.25)], linewidth=0.1, linecolor='red', endstyle='>')
+p += Line(points=[(2, 6.25), (6.25, 5.75)], linewidth=0.1, linecolor='red', endstyle='>')
 
 print(p)
 
