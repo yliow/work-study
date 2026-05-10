039bcce66e46ca6096efdb5331479627505a73e6
nalahaideb <nasseralahaideb@hotmail.com>
2026-05-09 23:05:35 -0500

    extract_exercises.py working, testing w compile. added clean_chapters.py to clean up chapters dir after extract_exercises.py

---
+++ courses/cpp-book/notes/clean_chapters.py
@@ -0,0 +1,13 @@
+#get rid of changes after running extract_exercise.py
+import os
+
+dir_ = os.getcwd() + '/chapters'
+
+for i in chapters:
+  try:
+    new_main = dir_ + '/' + i + '/new_main.tex'
+    old_main = dir_ + '/' + i + '/old_main.tex'
+    os.system('rm %s' % new_main)
+    os.system('rm %s' % old_main)
+  except Exception as e:
+    print("ERROR, CANT REMOVE FILE %s, MESSAGE %s" % (dir_ + '/' + i, e))
--- courses/cpp-book/notes/extract_exercises.py
+++ courses/cpp-book/notes/extract_exercises.py
@@ -20,49 +20,36 @@ import os
 '''
 ex_dest = file location string
 note = specific chapter string
-ex_content = ordered list of strings
+ex_content = ordered list of strings representing the exercise
 number = int ID of exercise
 '''
+
 def extraction_to_file(ex_dest, note, ex_content, number):
   #formatting for file/console printout, should just be 'num-chapter' 
   note = note.replace('/main.tex', '')
   
-  #check if note exercise folder exists
-  if not os.path.exists(ex_dest):
-    print("CREATING EXERCISE FOLDER FOR %s" % note)
-    os.system("mkdir %s" % ex_dest)
-
   #directory for exercise and possible soln
-  ex_folder = ex_dest + str(number) + '-' + note + '/'
   ex_console_name = ex_folder.replace(ex_dest, '')
-  
-  #add check for same content/file name
-  if os.path.exists(ex_folder):
-    print("EXERCISE FOLDER %s ALREADY EXISTS, EXITING..." % ex_console_name)
-    return
-    # num_exercises = len(os.listdir(ex_dest))
-    # number = num_exercises + 1
-    # ex_folder = ex_dest + str(number) + note
-    # print("RENAMING TO %s" % ex_folder.replace(ex_dest, ''))
 
-  #write exericse to file
-
-  #assign file name
-  ex_file = ex_folder + str(number) + '-' + note + '.tex'
+  #'solutions' works off the local dir so we change to that dir, we change back on return to read_file
+  # print("CHANGING TO: %s" % ex_dest)
   
-  if os.path.exists(ex_folder):
-    print("EXERCISE FOLDER FOR %s ALREADY EXISTS" % ex_file.replace(ex_folder, ''))
-  else:
-    os.system("mkdir %s" % ex_folder)
-  
-  print("WRITING EXERCISE TO %s" % ex_file)
+  os.chdir(ex_dest)
 
+  #assign file name and string for question.tex
+  ex_file = str(number) + '-' + note
+  question = ex_file + '/question.tex'
+  
+  #call solutions for this exercise
+  os.system("solutions make-ex %s" % ex_file)
+  
   try:
-    with open(ex_file, 'w') as x:
+    with open(question, 'w') as x:
+      x.write("\\tinysidebar{\\debug{exercises/{test/question.tex}}}\n")
       for entry in ex_content:
         x.write(entry)
   except Exception as e:
-    print("ERROR WRITING EXERCISE %s TO FILE %s" % (ex_file, e))
+    print("!!!!!!! ERROR WRITING EXERCISE %s TO FILE %s" % (question, e))
     ex_file = None
   return ex_file
 
@@ -87,11 +74,13 @@ def read_file(note, ex_dest, chap_dir):
         #write to file, if its None then an error occurred writing so we abandon the chapter
         exercise_file_str = extraction_to_file(ex_dest, note.replace(chap_dir, ''), ex_accumulator, counter)
         
+        #return to operating directory
+        # print(">>>>CHANGING FROM: %s" % chap_dir.replace('/chapters/', ''))
+        os.chdir(chap_dir.replace('/chapters', ''))
         #reset ex_accumulator
         ex_accumulator = []
-
         counter += 1
-
+        
         if exercise_file_str != None:
           #process result
           new_note.append("\\input{%s}" % exercise_file_str)
@@ -108,7 +97,7 @@ def read_file(note, ex_dest, chap_dir):
       #not in an exercise block, append line 
       else:
         new_note.append(line)
-  return new_note
+  return new_note, counter
 
 #directory references
 dir_ = os.getcwd()
@@ -127,14 +116,14 @@ for chapter in notes_files:
   print("working on chapter: %s" % chapter)
   try:
     note = chap_dir + chapter + '/main.tex'
-    note_exercises = ex_folder + chapter + '/'
 
     # assign and write new note to file
-    new_note = read_file(note, note_exercises, chap_dir)
+    new_note, counter = read_file(note, ex_folder, chap_dir)
     new_note_location = chap_dir + chapter + '/new_main.tex'
     with open(new_note_location, 'w') as new_dest:
       for line in new_note:
+        print("writing new_main.tex for chapter %s, extracted %s exercises to file\n" % (chapter, counter))
         new_dest.write(line)
   except Exception as e:
-    print("ERROR PROCESSING CHAPTER: %s, MESSAGE: %s" % (chapter, e))
+    print("!!!!!! ERROR PROCESSING CHAPTER: %s, MESSAGE: %s\n" % (chapter, e))
     
