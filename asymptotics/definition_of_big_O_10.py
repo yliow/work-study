53f28339bed61be25741ce96eafef075aa75ed81
Yihsiang Liow <yliow@ccis.edu>
2026-05-13 12:20:10 -0700

    no msg

--- asymptotics/definition_of_big_O_10.py
+++
@@ -1,5 +0,0 @@
-from latextool_basic import *
-p = FunctionPlot(domain='1:200', vars=['n'])
-p.add('3*n**2 + 5 + 10*n * sin(n)', color='red', python=1, pin='left', num_points=200)
-p.add('50*n', color='blue', python=1, pin='above', num_points=2)
-print(p)
--- asymptotics/definition_of_big_O_7.py
+++
@@ -1,5 +0,0 @@
-from latextool_basic import *
-p = FunctionPlot(domain='0:1', vars=['n'])
-p.add('3*n**2+5+10*n*sin(n)', color='red', python=1, pin='below')
-p.add('50*n', color='blue', python=1, pin='left', num_points=2)
-print(p)
