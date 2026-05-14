5988c02b0af93451d08d37a761ca28c02081f95d
nalahaideb <nasseralahaideb@hotmail.com>
2026-05-14 17:50:25 -0500

    forgot to remove old assignment_tool from main dir, moved to projects

--- assignment_tool/src/new/a02/Old/q01/Fraction.h
+++
@@ -1,7 +0,0 @@
-// File: Fraction.h
-// Author: smaug
-
-//-----------------------------------------------------------------------------
-// Print fraction modeled by numerator n and denominator d.
-//-----------------------------------------------------------------------------
-void Fraction_print(int, int);
